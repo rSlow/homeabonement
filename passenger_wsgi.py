ec82f8950abccff5183d69fd4926f82755003ddd
rslow <redmaxx4@gmail.com>
2023-06-26 15:05:06 +0900

    remove check_env, rebuild passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,9 +1,6 @@
 import sys
 import os
-import subprocess
 from pathlib import Path
-
-from config.utils import check_env
 
 BASE_DIR = Path(__file__).resolve().parent
 
@@ -12,15 +9,7 @@ INTERP = os.path.expanduser(BASE_DIR / "venv" / "bin" / "python3")
 if sys.executable != INTERP:
     os.execl(INTERP, INTERP, *sys.argv)
 
-from environs import Env
-ENV = Env()
-ENV.read_env(str(BASE_DIR / "env" / "mysql.env"))
-ENV.read_env(str(BASE_DIR / "env" / "django.env"))
-
-check_env()
-
-DJANGO_DEBUG = ENV.bool("DJANGO_DEBUG", default=False)
-if DJANGO_DEBUG is False:
-    subprocess.run(["python3", "manage.py", "collectstatic", "--clear", "--noinput"])
-
-from config.wsgi import application
+try:
+    from config.wsgi import application
+except Exception as ex:
+    raise
