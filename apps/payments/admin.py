3b4db2a4573e568366bbe372e511ddfe6507e2a6
rslow <redmaxx4@gmail.com>
2023-07-10 10:28:44 +0900

    edit payments admin, fix change admin payments form

--- apps/payments/admin.py
+++ apps/payments/admin.py
@@ -7,7 +7,8 @@ from .models import PaymentModel
 
 @admin.register(PaymentModel)
 class PaymentAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in PaymentModel._meta.get_fields()] + ['send_check']
+    list_display = ['payment_id', 'user', 'amount', 'description', 'is_paid', 'create_date', 'close_date',
+                    'is_check_sent', 'check_file', 'send_check']
     list_filter = ["is_paid", "is_check_sent"]
     search_fields = ["user__email"]
     change_form_template = 'admin/payments/change.html'
@@ -15,7 +16,8 @@ class PaymentAdmin(admin.ModelAdmin):
     def render_change_form(self, request, context, add=False, change=False, form_url="", obj=None):
         if context is None:
             context = {}
-        context["send_cheque_url"] = reverse_lazy("send_check", kwargs={'payment_id': obj.payment_id})
+        if obj and change:
+            context["send_cheque_url"] = reverse_lazy("send_check", kwargs={'payment_id': obj.payment_id})
         return super().render_change_form(request, context, add, change, form_url, obj)
 
     def send_check(self, obj: PaymentModel):
